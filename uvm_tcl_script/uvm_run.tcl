663a382955d9066d2f4f4a2ed21f001aa10267f9
24x7fpga <kiran.24x7fpga@gmail.com>
2024-08-29 11:56:55 -0400

    first commit

---
+++ uvm_tcl_script/uvm_run.tcl
@@ -0,0 +1,37 @@
+puts "Run Simulation TCL!"
+# run_simulation.tcl
+
+# Check if the correct number of arguments are passed
+if { [llength $argv] != 2 } {
+    puts "Usage: vivado -mode batch -source uvm_run.tcl -tclargs <project_name> <project_location>"
+    exit 1
+}
+
+# Get the project name and location from the arguments
+set project_name [lindex $argv 0]
+set project_location [lindex $argv 1]
+
+# Print the received arguments
+puts "Project Name: $project_name"
+puts "Project Location: $project_location"
+
+
+# Create a new Vivado project
+create_project $project_name $project_location/verif -part xc7z020clg400-1 -force
+
+# Add Source files
+add_files [glob $project_location/*.sv]
+
+# Set UVM properties
+set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+
+set_property top test [get_filesets sim_1]
+set_property top_lib xil_defaultlib [get_filesets sim_1]
+
+
+#set_property top test [current_fileset]
+#set_property top_file {$project_location/$project_name.sv} [current_fileset]
+
+# Start Simulation
+launch_simulation
---
+++ uvm_verification/test/test.sv
@@ -0,0 +1,11 @@
+`include "uvm_macros.svh"
+
+import uvm_pkg::*;
+
+module test;
+
+initial begin
+	`uvm_info("RUN_TEST", "Vivado Script Simulation Test", UVM_NONE);
+end
+	
+endmodule
