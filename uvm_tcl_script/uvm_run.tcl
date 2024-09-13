297c4356ed32ac92e780f0bf8f35a270fa2fe6ff
24x7fpga <kiran.24x7fpga@gmail.com>
2024-09-13 12:21:43 -0400

    updated run tcl to choose tb_{project_name}

--- uvm_tcl_script/uvm_run.tcl
+++ uvm_tcl_script/uvm_run.tcl
@@ -26,7 +26,7 @@ add_files [glob $project_location/*.sv]
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 
-set_property top test [get_filesets sim_1]
+set_property top tb_$project_name [get_filesets sim_1]
 set_property top_lib xil_defaultlib [get_filesets sim_1]
 
 
